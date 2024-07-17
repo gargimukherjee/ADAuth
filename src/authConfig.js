f466cadc3770139005dfeab04df5f332c3285bc8
unknown <design.gargi@gmail.com>
2024-07-17 17:38:33 +0530

    test

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,69 +1,25 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
+import { PublicClientApplication } from "@azure/msal-browser";
 
-import { LogLevel } from "@azure/msal-browser";
 
-/**
- * Configuration object to be passed to MSAL instance on creation. 
- * For a full list of MSAL.js configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
- */
 export const msalConfig = {
     auth: {
-        clientId: '7aea7368-6742-40c4-a6a4-d84de6e32eb4', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://login.microsoftonline.com/d37b19a4-5644-48b8-b879-66c5480183b6', // Defaults to "https://login.microsoftonline.com/common"
-        redirectUri: '/', // Points to window.location.origin. You must register this URI on Azure Portal/App Registration.
-        postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
-        navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+        clientId: "",
+        authority: "https://login.microsoftonline.com/",
+        redirectUri: window.location.origin,
+        postLogoutRedirectUri:`${window.location.origin}/logout`
     },
     cache: {
-        cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
-        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback: (level, message, containsPii) => {
-                if (containsPii) {
-                    return;
-                }
-                switch (level) {
-                    case LogLevel.Error:
-                        console.error(message);
-                        return;
-                    case LogLevel.Info:
-                        console.info(message);
-                        return;
-                    case LogLevel.Verbose:
-                        console.debug(message);
-                        return;
-                    case LogLevel.Warning:
-                        console.warn(message);
-                        return;
-                    default:
-                        return;
-                }
-            },
-        },
-    },
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true, 
+    }
 };
 
-/**
- * Scopes you add here will be prompted for user consent during sign-in.
- * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
- * For more information about OIDC scopes, visit: 
- * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
- */
 export const loginRequest = {
-    scopes: []
+    scopes: ["user.read"]
 };
 
-/**
- * An optional silentRequest object can be used to achieve silent SSO
- * between applications by providing a "login_hint" property.
- */
-export const silentRequest = {
-    scopes: ["openid", "profile"],
-    loginHint: "example@domain.net"
+export const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
 };
+
+export const msalInstance = new PublicClientApplication(msalConfig);
