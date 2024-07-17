0e19279a7d64ebadc719ffdd341d41d8c975f045
unknown <design.gargi@gmail.com>
2024-07-17 19:32:06 +0530

    blah

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,8 +3,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 export const msalConfig = {
     auth: {
-        clientId: "",
-        authority: "https://login.microsoftonline.com/",
+        clientId: "88f76bd3-446a-45e5-af27-674fc7853cec",
+        authority: "https://login.microsoftonline.com/d37b19a4-5644-48b8-b879-66c5480183b6",
         redirectUri: window.location.origin,
         postLogoutRedirectUri:`${window.location.origin}/logout`
     },
